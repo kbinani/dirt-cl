e4fa1089df503ad69f95deb0bc28d488fb284314
kbinani <kbinani.bt@gmail.com>
2025-08-14 23:57:58 +0900

    feat: Add DirtRotation function

--- src/kernel.cl
+++ src/kernel.cl
@@ -1,2 +1,93 @@
-__kernel void run() {
+#if defined(__OPENCL_C_VERSION__ )
+typedef long i64;
+typedef int i32;
+#define MULTIPLIER (0x5DEECE66DL)
+#define ADDEND (0xBL)
+#define MASK ((1L << 48) - 1L)
+#define LONG(v) (v##L)
+#else
+typedef int64_t i64;
+typedef int32_t i32;
+#define MULTIPLIER (0x5DEECE66DLL)
+#define ADDEND (0xBLL)
+#define MASK ((1LL << 48) - 1LL)
+#define LONG(v) (v##LL)
+#endif
+
+static i64 Rand(i64 seed) {
+  return (seed ^ MULTIPLIER) & MASK;
 }
+
+static i32 Next(i64 *state, i32 bits) {
+  *state = (*state * MULTIPLIER + ADDEND) & MASK;
+  return (i32)(*state >> (48 - bits));
+}
+
+static i32 NextInt(i64 *state) {
+  return Next(state, 32);
+}
+
+static i32 NextIntBounded(i64* state, int n) {
+  if ((n & -n) == n) {  // i.e., n is a power of 2
+    return (i32)((n * (i64)Next(state, 31)) >> 31);
+  }
+
+  i32 bits;
+  i32 val;
+  do {
+    bits = Next(state, 31);
+    val = bits % n;
+  } while (bits - val + (n - 1) < 0);
+  return val;
+}
+
+static i64 NextLong(i64 *state) {
+  i64 ret = ((i64)Next(state, 32)) << 32;
+  ret += Next(state, 32);
+  return ret;
+}
+
+static i64 GetPositionSeed(i32 x, i32 y, i32 z) {
+  i64 i = (i64)(x * LONG(3129871)) ^ (i64)z * LONG(116129781) ^ (i64)y;
+  i = i * i * LONG(42317861) + i * LONG(11);
+  return i >> 16;
+}
+
+static i32 GetRandomItemIndex(i32 totalWeight, i32 weight) {
+  for (i32 i = 0; i < totalWeight; i++) {
+    i32 t = i;
+    weight -= 1;
+    if (weight < 0) {
+      return t;
+    }
+  }
+  return -1;
+}
+
+static i32 Abs(i32 v) {
+  if (v < 0) {
+    return -v;
+  } else {
+    return v;
+  }
+}
+
+static i32 DirtRotation(i32 x, i32 y, i32 z, i32 dataVersion) {
+  i64 seed = GetPositionSeed(x, y, z);
+  i64 state = Rand(seed);
+  i32 numFacingTypes = 4;
+  if (dataVersion >= 4063) {
+    // 24w36a
+    return NextIntBounded(&state, numFacingTypes);
+  } else {
+    // 24w35a
+    i32 weight = Abs((i32)NextLong(&state)) % numFacingTypes;
+    return GetRandomItemIndex(numFacingTypes, weight);
+  }
+}
+
+#if defined(__OPENCL_C_VERSION__)
+__kernel void run(__global i32 *rot, i32 x, i32 y, i32 z, i32 dataVersion) {
+  *rot = DirtRotation(x, y, z, dataVersion);
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,14 @@
 #include <string>
 #include <string_view>
 
+#include "./kernel.cl"
+
 using namespace std;
+
+template <std::integral T>
+cl_int SetKernelArg(cl_kernel kernel, size_t index, T value) {
+  return clSetKernelArg(kernel, index, sizeof(T), &value);
+}
 
 int main(int argc, char* argv[]) {
   string cl;
@@ -43,53 +50,53 @@ int main(int argc, char* argv[]) {
 
   cout << "Platforms:" << endl;
   cl_uint numPlatforms;
-  if (clGetPlatformIDs(0, nullptr, &numPlatforms) != 0) {
+  if (clGetPlatformIDs(0, nullptr, &numPlatforms) != CL_SUCCESS) {
     return 1;
   }
   if (numPlatforms < 1) {
     return 1;
   }
   vector<cl_platform_id> platforms(numPlatforms);
-  if (clGetPlatformIDs(numPlatforms, platforms.data(), nullptr) != 0) {
+  if (clGetPlatformIDs(numPlatforms, platforms.data(), nullptr) != CL_SUCCESS) {
     return 1;
   }
   for (size_t i = 0; i < platforms.size(); i++) {
     cl_platform_id id = platforms[i];
     size_t size = 0;
-    if (clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, nullptr, &size) != 0) {
+    if (clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, nullptr, &size) != CL_SUCCESS) {
       return 1;
     }
-    vector<char> name(size + 1, '0');
-    if (clGetPlatformInfo(id, CL_PLATFORM_NAME, name.size(), name.data(), nullptr) != 0) {
+    string name(size + 1, '0');
+    if (clGetPlatformInfo(id, CL_PLATFORM_NAME, name.size(), name.data(), nullptr) != CL_SUCCESS) {
       return 1;
     }
-    cout << "  #" << i << " " << name.data() << endl;
+    cout << "  #" << i << " " << name.c_str() << endl;
   }
   cl_platform_id platform = platforms[0];
 
   cout << "Devices:" << endl;
   cl_uint numDevices;
-  if (clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices) != 0) {
+  if (clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices) != CL_SUCCESS) {
     return 1;
   }
   if (numDevices < 1) {
     return 1;
   }
   vector<cl_device_id> devices(numDevices);
-  if (clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, numDevices, devices.data(), nullptr) != 0) {
+  if (clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, numDevices, devices.data(), nullptr) != CL_SUCCESS) {
     return 1;
   }
   for (size_t i = 0; i < devices.size(); i++) {
     cl_device_id id = devices[i];
     size_t size;
-    if (clGetDeviceInfo(id, CL_DEVICE_NAME, 0, nullptr, &size) != 0) {
+    if (clGetDeviceInfo(id, CL_DEVICE_NAME, 0, nullptr, &size) != CL_SUCCESS) {
       return 1;
     }
-    vector<char> name(size);
-    if (clGetDeviceInfo(id, CL_DEVICE_NAME, size, name.data(), nullptr) != 0) {
+    string name(size + 1, '0');
+    if (clGetDeviceInfo(id, CL_DEVICE_NAME, size, name.data(), nullptr) != CL_SUCCESS) {
       return 1;
     }
-    cout << "  #" << i << " " << name.data() << endl;
+    cout << "  #" << i << " " << name.c_str() << endl;
   }
   cl_device_id device = devices[0];
 
@@ -115,7 +122,16 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  if (clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr) != 0) {
+  if (clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr) != CL_SUCCESS) {
+    size_t size;
+    if (clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &size) != CL_SUCCESS) {
+      return 1;
+    }
+    string log(size + 1, '0');
+    if (clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, size, log.data(), nullptr) != CL_SUCCESS) {
+      return 1;
+    }
+    cout << log.c_str() << endl;
     return 1;
   }
 
@@ -125,14 +141,47 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  if (clEnqueueTask(queue, kernel, 0, nullptr, nullptr) != 0) {
+  err = 0;
+  cl_mem mem = clCreateBuffer(ctx, CL_MEM_READ_WRITE, sizeof(int), nullptr, &err);
+  if (err != 0) {
     return 1;
   }
+  int x = 0;
+  int y = 62;
+  int z = 0;
+  int dataVersion = 4063;
+  if (clSetKernelArg(kernel, 0, sizeof(mem), &mem) != CL_SUCCESS) {
+    return 1;
+  }
+  if (SetKernelArg(kernel, 1, x) != CL_SUCCESS) {
+    return 1;
+  }
+  if (SetKernelArg(kernel, 2, y) != CL_SUCCESS) {
+    return 1;
+  }
+  if (SetKernelArg(kernel, 3, z) != CL_SUCCESS) {
+    return 1;
+  }
+  if (SetKernelArg(kernel, 4, dataVersion) != CL_SUCCESS) {
+    return 1;
+  }
+
+  if (clEnqueueTask(queue, kernel, 0, nullptr, nullptr) != CL_SUCCESS) {
+    return 1;
+  }
+
+  vector<int> buffer(1);
+  if (clEnqueueReadBuffer(queue, mem, CL_TRUE, 0, sizeof(int), buffer.data(), 0, nullptr, nullptr) != CL_SUCCESS) {
+    return 1;
+  }
+  cout << "[" << x << ", " << y << ", " << z << "]: " << buffer[0] << endl;
+  cout << "expected: " << DirtRotation(x, y, z, dataVersion) << endl;
 
   clFlush(queue);
   clFinish(queue);
   clReleaseKernel(kernel);
   clReleaseProgram(program);
+  clReleaseMemObject(mem);
   clReleaseCommandQueue(queue);
   clReleaseContext(ctx);
   return 0;
